58b10bf0de3694a89fd85f3abfa33d69d8d2dfc9
Nicknjano <nnjano254@gmail.com>
2023-02-21 21:53:44 +0300

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+  *main -prints alphabet in lower case
+  *Return: always 0 success
+  */
+int main(void)
+{
+	for(i ='a';i<='z';i++)
+	{
+		putchar("i");
+	}
+	putchar("\n");
+	return (0);
+}
