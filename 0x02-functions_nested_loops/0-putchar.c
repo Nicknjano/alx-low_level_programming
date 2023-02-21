4a736aef3c54f5ddf52acc4e7ab54832c32cbe25
Nicknjano <nnjano254@gmail.com>
2023-02-21 21:32:45 +0300

    _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+  *main - prints _putchar
+  *Return: always succes.
+  */
+int main(void)
+{
+	char word[9] = "_putchar";
+	int i;
+
+	for (i = 0; i < 9; i++)
+		_putchar(word[i]);
+	_putchar('\n');
+	return (0);
+}
+
