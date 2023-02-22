8209b70763255f70c3c3ff9b094b1f858ec602d7
Nicknjano <122860272+Nicknjano@users.noreply.github.com>
2023-02-22 13:15:27 +0300

    Update 5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -4,6 +4,7 @@
   *print_sign -check main.h
   *@n :an integer variable
   *Description -checks whether n>0,n<0,n==0
+  *Return: 1 if number is positive, 0 if number is 0, -1 if number is negative
   */
 int print_sign(int n)
 {
