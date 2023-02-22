39e2f473692c570e80db834e1829e0724944d11b
Nicknjano <nnjano254@gmail.com>
2023-02-22 15:24:32 +0300

    times_table

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+  *@a: integer variable
+  *@b: integer variable
+  *int add - check main.h
+  *Return: result of a+b
+  */
+int add(int a, int b)
+{
+	int result;
+
+	result = a + b ;
+	_putchar(result + '\n');
+	return (result);
+}
