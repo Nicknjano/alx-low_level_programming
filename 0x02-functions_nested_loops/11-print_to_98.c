7be7fe049003f839c6f15dd47d4e6d8f83f6c274
Nicknjano <nnjano254@gmail.com>
2023-02-22 15:46:10 +0300

    n_to_98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdlib>
+/**
+  *@n: starting number
+  *print_to_98 -prints from  n to 98
+  *Return: nothing
+  */
+void print_to_98(int n)
+{
+	int i;
+	for (i = n; i <= 98; i++)
+	{
+		_putchar(i + ', ');
+	}
+}
