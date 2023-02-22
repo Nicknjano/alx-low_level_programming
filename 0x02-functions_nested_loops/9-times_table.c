b71b4f6432c90e58faf79f418eac28837955de1a
Nicknjano <nnjano254@gmail.com>
2023-02-22 15:05:38 +0300

    times_table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+  *times_table - check main.h
+  *Dexcription: print 9times table
+  *Return: nothing
+  */
+void times_table(void)
+{
+	int result;
+	int i;
+	int j;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			result = i * j;
+			_putchar(result+','+' ');
+		}
+		_putchar('\n');
+	}
+}
