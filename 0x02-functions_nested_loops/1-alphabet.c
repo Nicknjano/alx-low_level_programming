24d8f0a4044e20e14fd3948f5aea92a319688118
Nicknjano <nnjano254@gmail.com>
2023-02-22 11:12:20 +0300

    new commit

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,12 +3,12 @@
   *main -prints alphabet in lower case
   *Return: nothing
   */
-int main(void)
+void print_alphabet(void)
 {
 	int i;
 
 	for(i ='a';i<='z';i++)
 		_putchar(i);
+
 	_putchar('\n');
-	return (0);
 }
