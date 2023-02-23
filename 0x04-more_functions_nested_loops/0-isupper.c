8b04c7f558a4d30b977b2aa66b30eb3c4c80b588
Nicknjano <nnjano254@gmail.com>
2023-02-23 21:17:54 +0300

    is upper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+  *-isupper -check main.h
+  *function checks whether c is uppercase
+  *@c: integer input
+  *Return: 1 if c is upper else 0
+  */
+int _isupper(int c)
+{
+	int value = 0;
+
+	for (i = 'A'; i <= 'Z'; i++)
+	{
+		if (i == c)
+			value = 1;
+	}
+	return (value);
+}
